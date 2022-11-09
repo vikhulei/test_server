4e561dfe87b8cbbcd0e1914613ffa41cb0f4a4d0
vikhulei <vikhulei@gmail.com>
2022-11-09 20:49:05 +0000

    server.js added

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require("express")
 const router = require("./api/getRequests")
-const port = 3000
 
 const app = express()
 
 app.locals.test = `<h2>test</h2>`
 
-app.listen(port, () => console.log(`listening on port ${port}`))
-
 app.use("/test", router)
+
+module.exports = app
---
+++ server.js
@@ -0,0 +1,4 @@
+const app = require("./index")
+const port = 3000
+
+app.listen(port, () => console.log(`listening on port ${port}`))
